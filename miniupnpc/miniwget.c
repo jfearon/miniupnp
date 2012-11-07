a18db1444d747a4d9fc62e1458f4c91d9358d784
James Fearon <james.fearon@gmail.com>
2012-11-07 22:15:09 +0000

    Add android definition

--- miniupnpc/miniwget.c
+++ miniupnpc/miniwget.c
@@ -43,7 +43,7 @@
  * during the connect() call */
 #define MINIUPNPC_IGNORE_EINTR
 #endif /* #else _WIN32 */
-#if defined(__sun) || defined(sun)
+#if defined(__sun) || defined(sun) || defined(ANDROID)
 #define MIN(x,y) (((x)<(y))?(x):(y))
 #endif
 
